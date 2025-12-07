8987995a244d35f6533996cbfc90217ffee21737
Nemanja Šoć <nemanja.soc@cyrillicsoftware.net>
2025-12-07 09:47:16 +0100

    add loggin page guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.component';
 import { HomePage } from './pages/home/home.component';
 import { AuthGuard } from './services/auth.guard';
+import { GuestGuard } from './services/guest.guard';
 import { PostDetailsPage } from './pages/post-details/post-details.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'login', pathMatch: 'full' },
-	{ path: 'login', component: LoginPage },
+	{ path: 'login', component: LoginPage, canActivate: [GuestGuard] },
 	{ path: 'home', component: HomePage, canActivate: [AuthGuard] }
 	, { path: 'posts/:id', component: PostDetailsPage }
 ];
